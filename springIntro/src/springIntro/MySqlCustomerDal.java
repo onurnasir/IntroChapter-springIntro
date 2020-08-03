4a47f992481173ca9af4b189147ccebae963ea7b
Onur Nasır <onur.nasr@gmail.com>
2020-08-04 00:10:38 +0300

    springIntro Example ( via Eclipse IDE ) Added.

---
+++ springIntro/src/springIntro/MySqlCustomerDal.java
@@ -0,0 +1,21 @@
+package springIntro;
+
+public class MySqlCustomerDal implements ICustomerDal{
+
+	String connectionString;
+	
+	public String getConnectionString() {
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+	
+	@Override
+	public void add() {
+		System.out.println("Mysql veritabanına eklendi");
+		
+	}
+
+}
